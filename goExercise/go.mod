759d8047db17c1dc9f86cfb51a4aa2acd73a4dde
leon123858 <a0970785699@gmail.com>
2023-10-21 00:18:38 +0800

    edit

--- goExercise/go.mod
+++ goExercise/go.mod
@@ -3,8 +3,8 @@ module project
 go 1.16
 
 require (
-	github.com/GenericP3rson/GoSamplePackage v0.0.11 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible
+	github.com/leon123858/go_exercise v0.0.1 // indirect
 )
 
 replace project/say => ./say
--- goExercise/main.go
+++ goExercise/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	mySamplePackage "github.com/leon123858/go_exercise/mypackage"
 )
 
 type node struct {
@@ -23,7 +24,7 @@ func reverse(source *string) {
 }
 
 func main() {
-
+	mySamplePackage.Fib(0)
 	fmt.Println("Start!")
 	var height string
 	fmt.Scanln(&height)
